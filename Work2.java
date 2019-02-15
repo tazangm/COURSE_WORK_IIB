c33f596ffd4eff320eb1f725cd7bf320c43a8aec
tazangm <46903222+tazangm@users.noreply.github.com>
2019-02-15 14:41:49 +0300

    Add files via upload

---
+++ Work2.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Work2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner scn = new Scanner(System.in);
+System.out.print("Student Name:"+"");
+	}
+
+}
